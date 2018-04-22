0a15d6812fd755ac87caf9f9d888c3b1a69376c9
Jerry <leeming-jiang@163.com>
2018-04-22 22:36:19 +0800

    remove jsonignore for auditing entity

--- src/main/java/com/pure/service/domain/AbstractAuditingEntity.java
+++ src/main/java/com/pure/service/domain/AbstractAuditingEntity.java
@@ -1,18 +1,17 @@
 package com.pure.service.domain;
 
-import java.io.Serializable;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.Instant;
+
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * Base abstract class for entities which will hold definitions for created, last modified by and created,
@@ -27,22 +26,22 @@ public abstract class AbstractAuditingEntity implements Serializable {
 
     @CreatedBy
     @Column(name = "created_by", nullable = false, length = 50, updatable = false)
-    @JsonIgnore
+//    @JsonIgnore
     private String createdBy;
 
     @CreatedDate
     @Column(name = "created_date", nullable = false)
-    @JsonIgnore
+//    @JsonIgnore
     private Instant createdDate = Instant.now();
 
     @LastModifiedBy
     @Column(name = "last_modified_by", length = 50)
-    @JsonIgnore
+//    @JsonIgnore
     private String lastModifiedBy;
 
     @LastModifiedDate
     @Column(name = "last_modified_date")
-    @JsonIgnore
+//    @JsonIgnore
     private Instant lastModifiedDate = Instant.now();
 
     public String getCreatedBy() {
