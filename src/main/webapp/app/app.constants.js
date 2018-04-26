dc8c0e02435848d12b0c37f2e3fbeb3fe2a80c77
Jerry <leeming-jiang@163.com>
2018-04-26 16:13:57 +0800

    update docker file

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('simpleServiceApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1524401916210)
+        .constant('BUILD_TIMESTAMP', 1524726006066)
 ;
 })();
