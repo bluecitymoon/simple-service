8358428e3fdd1f5d8b5012120fbd77eeb62b5227
Jerry <leeming-jiang@163.com>
2018-05-25 14:08:06 +0800

    update js file folder

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('simpleServiceApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1526609351678)
+        .constant('BUILD_TIMESTAMP', 1527228106783)
 ;
 })();
