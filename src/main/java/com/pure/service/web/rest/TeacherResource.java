b0136047835ea28eb42ab62c14abd5329aeac928
Jerry <leeming-jiang@163.com>
2019-01-02 21:37:07 +0800

    shared teacher

--- src/main/java/com/pure/service/web/rest/TeacherResource.java
+++ src/main/java/com/pure/service/web/rest/TeacherResource.java
@@ -3,14 +3,13 @@ package com.pure.service.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import com.pure.service.domain.Teacher;
 import com.pure.service.region.RegionBasedInsert;
-import com.pure.service.region.RegionBasedQuery;
+import com.pure.service.service.TeacherQueryService;
 import com.pure.service.service.TeacherService;
+import com.pure.service.service.dto.TeacherCriteria;
 import com.pure.service.web.rest.util.HeaderUtil;
 import com.pure.service.web.rest.util.PaginationUtil;
-import com.pure.service.service.dto.TeacherCriteria;
-import com.pure.service.service.TeacherQueryService;
-import io.swagger.annotations.ApiParam;
 import io.github.jhipster.web.util.ResponseUtil;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -18,11 +17,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -98,7 +103,7 @@ public class TeacherResource {
      */
     @GetMapping("/teachers")
     @Timed
-    @RegionBasedQuery
+//    @RegionBasedQuery
     public ResponseEntity<List<Teacher>> getAllTeachers(TeacherCriteria criteria,@ApiParam Pageable pageable) {
         log.debug("REST request to get Teachers by criteria: {}", criteria);
         Page<Teacher> page = teacherQueryService.findByCriteria(criteria, pageable);
