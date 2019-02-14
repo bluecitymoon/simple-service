c54724186459160490d998e7bb1ab6869e8607c3
Jerry <leeming-jiang@163.com>
2019-02-14 15:02:08 +0800

    refresh teacher connection to class arrangement while the teacher is changed in the class

--- src/main/java/com/pure/service/domain/ClassArrangement.java
+++ src/main/java/com/pure/service/domain/ClassArrangement.java
@@ -27,7 +27,7 @@ import java.util.Objects;
         query = "select  ca.id as arrangementId, p.id as classId, concat(DATE_FORMAT(start_date,'%H:%i'), '-', DATE_FORMAT(end_date,'%H:%i'),  ' ', p.name, ' ', t.name) as title, p.name as className, p.students_reach_max_numbers as fullTag, cs.name as statusTag, t.name as teacherName, ca.start_date as start, ca.end_date as end, cr.name as classroomName, cr.id as classroomId, c.name as courseName\n" +
             " from class_arrangement ca\n" +
             " cross join product p on ca.clazz_id = p.id\n" +
-            " cross join teacher t on ca.planed_teacher_id = t.id\n" +
+            " cross join teacher t on p.teacher_id = t.id\n" +
             " cross join class_room cr on p.class_room_id = cr.id\n" +
             " cross join course c on p.course_id = c.id\n" +
             " left join class_status cs on cs.id = p.status_id \n" +
@@ -37,7 +37,7 @@ import java.util.Objects;
         query = "select  ca.id as arrangementId, p.id as classId, concat(DATE_FORMAT(start_date,'%H:%i'), '-', DATE_FORMAT(end_date,'%H:%i'),  ' ', p.name, ' ', t.name) as title, p.name as className, p.students_reach_max_numbers as fullTag, cs.name as statusTag, t.name as teacherName, ca.start_date as start, ca.end_date as end, cr.name as classroomName, cr.id as classroomId, ccb.name as courseName\n" +
             " from class_arrangement ca\n" +
             " cross join product p on ca.clazz_id = p.id\n" +
-            " cross join teacher t on ca.planed_teacher_id = t.id\n" +
+            " cross join teacher t on p.teacher_id = t.id\n" +
             " cross join class_room cr on p.class_room_id = cr.id\n" +
             " cross join course c on p.course_id = c.id\n" +
             " left join class_category_base ccb on c.class_category_base_id = ccb.id\n" +
--- src/main/java/com/pure/service/service/impl/ProductServiceImpl.java
+++ src/main/java/com/pure/service/service/impl/ProductServiceImpl.java
@@ -1,10 +1,14 @@
 package com.pure.service.service.impl;
 
+import com.pure.service.domain.ClassArrangement;
 import com.pure.service.domain.Product;
 import com.pure.service.region.RegionUtils;
 import com.pure.service.repository.ProductRepository;
+import com.pure.service.service.ClassArrangementQueryService;
+import com.pure.service.service.ClassArrangementService;
 import com.pure.service.service.ProductQueryService;
 import com.pure.service.service.ProductService;
+import com.pure.service.service.dto.ClassArrangementCriteria;
 import com.pure.service.service.dto.ProductCriteria;
 import io.github.jhipster.service.filter.LongFilter;
 import io.github.jhipster.service.filter.StringFilter;
@@ -33,6 +37,12 @@ public class ProductServiceImpl implements ProductService{
 
     @Autowired
     private ProductQueryService productQueryService;
+
+    @Autowired
+    private ClassArrangementQueryService classArrangementQueryService;
+
+    @Autowired
+    private ClassArrangementService classArrangementService;
 
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -69,6 +79,32 @@ public class ProductServiceImpl implements ProductService{
                 throw new RuntimeException("该编号已存在");
             }
 
+        }
+
+        else {
+
+            Product originalClass = findOne(product.getId());
+
+            //班级老师发生变化时，需要将排课老师更新
+            if (originalClass.getTeacher() != null && product.getTeacher() != null && originalClass.getTeacher() != product.getTeacher()) {
+
+                ClassArrangementCriteria classArrangementCriteria = new ClassArrangementCriteria();
+
+                LongFilter clazzIdFilter = new LongFilter();
+                clazzIdFilter.setEquals(product.getId());
+
+                classArrangementCriteria.setClazzId(clazzIdFilter);
+
+                List<ClassArrangement> classArrangements = classArrangementQueryService.findByCriteria(classArrangementCriteria);
+                for (ClassArrangement classArrangement : classArrangements) {
+
+                    if (classArrangement.getActualTeacher() == null) {
+
+                        classArrangement.setPlanedTeacher(product.getTeacher());
+                        classArrangementService.save(classArrangement);
+                    }
+                }
+            }
         }
 
         return productRepository.save(product);
